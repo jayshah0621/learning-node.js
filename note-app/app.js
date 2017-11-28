ca2084e6743e8a83a9121f15ee8e070120b2c95f
jayshah0621 <30275758+jayshah0621@users.noreply.github.com>
2017-11-27 20:08:46 -0500

    Update app.js
    Code cleanup

--- note-app/app.js
+++ note-app/app.js
@@ -1,5 +1,3 @@
-console.log("Starting app.js");
-
 const fs 	= require('fs');
 const _ 	= require('lodash');
 const yargs = require('yargs');
@@ -9,7 +7,6 @@ const notes = require('./notes');
 const argv 	= yargs.argv;
 
 var command = argv._[0];
-console.log(`Performing Operation: ${command}.`);
 
 switch( command ) {
 	case 'add':
