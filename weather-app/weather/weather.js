7e0cb9554486179e2873f63de7f6fdf845754ab0
jayshah0621 <30275758+jayshah0621@users.noreply.github.com>
2017-11-28 19:40:21 -0500

    Update weather.js
    Remove callback. Add Promise.

--- weather-app/weather/weather.js
+++ weather-app/weather/weather.js
@@ -1,20 +1,22 @@
 const request = require('request');
 
-var getWeather = ( lat, lng, callback ) => {
-	request( {
-		url: `https://api.darksky.net/forecast/25e3e95ea74479851f6efd107fa34448/${lat},${lng}`,
-		json: true
-	}, (error, response, body) => {
-		if( !error && response.statusCode === 200 ) {
-			callback( undefined, {
-				temperature: body.currently.temperature,
-				feelsLike: body.currently.apparentTemperature
-			} );
-		} else {
-			callback( `Either there was an error or couldn't connect to the server. Please try again.`  );
-		}
+var getWeather = ( lat, lng ) => {
+	return new Promise( ( resolve, reject ) => {
+		request( {
+			url: `https://api.darksky.net/forecast/25e3e95ea74479851f6efd107fa34448/${lat},${lng}`,
+			json: true
+		}, (error, response, body) => {
+			if( !error && response.statusCode === 200 ) {
+				resolve( {
+					temperature: body.currently.temperature,
+					feelsLike: body.currently.apparentTemperature
+				} );
+			} else {
+				reject( `Either there was an error or couldn't connect to the server. Please try again.`  );
+			}
+		} );
 	} );
-}
+};
 
 module.exports = {
 	getWeather
