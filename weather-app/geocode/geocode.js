adc584e5ce9b07e568e686cbe5271373297e7d54
jayshah0621 <30275758+jayshah0621@users.noreply.github.com>
2017-11-28 19:39:43 -0500

    Update geocode.js
    Remove callbacks. Add Promise.

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -1,30 +1,28 @@
 const request = require('request');
 
-var geocodeAddress = ( address, callback ) => {
+var geocodeAddress = ( address ) => {
 	var encodedAddress = encodeURIComponent( address );
 
-	request( {
-		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-		json: true
-	}, (error, response, body) => {
-		if( error ) {
-			callback( "Something went wrong. Please try again." );
-		} else if( response.statusCode !== 200 ) {
-			callback( "Unable to connect to the URL." );
-		} else if( body.status === 'ZERO_RESULTS' ) {
-			callback( "Unable to find the address. Please try a valid address." );
-		} else if( body.status === 'OK' ) {
-			callback( undefined, {
-				address: body.results[0].formatted_address,
-				latitude: body.results[0].geometry.location.lat,
-				longitude: \
-			} );
-			console.log( `Address: ${body.results[0].formatted_address}` );
-			console.log( `Lat: ${body.results[0].geometry.location.lat}` );
-			console.log( `Long: ${body.results[0].geometry.location.lng}` );	
-		}
+	return new Promise( ( resolve, reject ) => {
+		request( {
+			url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+			json: true
+		}, (error, response, body) => {
+			if( error ) {
+				reject( "Something went wrong. Please try again." );
+			} else if( response.statusCode !== 200 ) {
+				reject( "Unable to connect to the URL." );
+			} else if( body.status === 'ZERO_RESULTS' ) {
+				reject( "Unable to find the address. Please try a valid address." );
+			} else if( body.status === 'OK' ) {
+				resolve( {
+					address: body.results[0].formatted_address,
+					latitude: body.results[0].geometry.location.lat,
+					longitude: body.results[0].geometry.location.lng
+				} );
+			}
+		} );
 	} );
-
 };
 
 module.exports = {
