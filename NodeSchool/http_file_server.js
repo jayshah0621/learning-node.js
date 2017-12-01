7ea7b0c5cc25bd09e843b058d443b4dd1881500e
jayshah0621 <30275758+jayshah0621@users.noreply.github.com>
2017-12-01 14:46:20 -0500

    Create http_file_server.js

---
+++ NodeSchool/http_file_server.js
@@ -0,0 +1,18 @@
+// NodeSchool - LearnYouNode - Challenge #11
+
+// Write an HTTP server that serves the same text file for each request it receives.
+// Your server should listen on the port provided by the first argument to your program.
+// You will be provided with the location of the file to serve as the second command-line argument. 
+// You must use the fs.createReadStream() method to stream the file contents to the response.
+
+const http = require('http');
+ const fs = require('fs');
+
+var port = process.argv[2];
+var file = process.argv[3];
+
+var server = http.createServer( ( req, res ) => {
+ 	fs.createReadStream( file ).pipe(res);
+} );
+
+server.listen( port );
