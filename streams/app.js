5814004afe44c3473bbf56fb5df0e9c48ef0eec5
jayshah0621 <30275758+jayshah0621@users.noreply.github.com>
2017-12-10 13:30:53 -0500

    Update app.js
    Replace 'on' event with 'pipe'

--- streams/app.js
+++ streams/app.js
@@ -1,13 +1,9 @@
 var fs = require('fs');
 
-var readable = fs.createReadStream('./lipsum.txt', { encoding: 'utf8', highWaterMark: 8*1024 });
+var readable = fs.createReadStream('./lipsum.txt');
 
 var writable = fs.createWriteStream('./lipsumcopy.txt', );
 
-readable.on('data', (data) => {
-    writable.write(data);
-});
+readable.pipe(writable);
 
-readable.on('end', () => {
-    console.log('Lipsum copied.');
-});
+console.log('Lipsum copied.');
